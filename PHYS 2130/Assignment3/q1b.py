55143e2d99fc3d30105c310c875f7ff91a687997
MrJeremyFisher <jeremy@generaleclectic.org>
2024-11-17 21:32:21 -0500

    More ps8 + 2130a3

---
+++ PHYS 2130/Assignment3/q1b.py
@@ -0,0 +1,46 @@
+import numpy as np
+import matplotlib.pyplot as pyplot
+
+
+def harm_osc_params(w_0: float,
+                    beta: float,
+                    A: float,
+                    w: float,
+                    x_0: float
+                    ) -> tuple[float, float, float, float, float]:
+    # "Given"
+    w_1 = np.sqrt(w_0**2-beta**2)
+    D = A/(np.sqrt((w_0**2-w**2)+4*(w**2)*beta**2))
+    delta = np.atan((2*w*beta)/(w_0**2-w**2))
+    
+    # Unknowns
+    phi = np.atan((beta-((D*w*np.sin(delta))/(x_0-D*np.cos(delta))))/w_1)
+    B = (x_0-D*np.cos(delta))/(np.cos(phi))
+
+    print(f"w_1: {w_1}", f"B: {B}", f"phi: {phi}", f"D: {D}", f"delta: {delta}", sep="\n")
+
+    return w_1, B, phi, D, delta
+
+
+def harm_osc_x_pos(w_0: float,
+                   beta: float,
+                   A: float,
+                   w: float,
+                   x_0: float,
+                   t: np.ndarray
+                   ) -> list:
+    w_1, B, phi, D, delta = harm_osc_params(w_0,
+                                            beta,
+                                            A,
+                                            w,
+                                            x_0)
+
+    return B*np.exp(-beta*t)*np.cos(w_1*t-phi)+D*np.cos(w*t-delta)
+
+
+t_vals = np.arange(0, 1000, 0.1)
+pyplot.plot(t_vals, harm_osc_x_pos(1, 0.01, 2, 0.1, 2, t_vals))
+pyplot.xlabel("Time (s)")
+pyplot.ylabel("Position (m)")
+pyplot.legend
+pyplot.show()
