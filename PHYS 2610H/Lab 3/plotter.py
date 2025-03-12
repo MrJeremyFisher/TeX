3e86c227496e8049ed5e8e9f575aa6586891c372
MrJeremyFisher <jeremy@generaleclectic.org>
2025-03-11 22:35:39 -0400

    More work

--- PHYS 2610H/Lab 3/plotter.py
+++ PHYS 2610H/Lab 3/plotter.py
@@ -2,23 +2,32 @@ from matplotlib import pyplot
 import numpy as np
 
 
-def plot_cal_norm(filename: str, save = False) -> None:
-    data = np.loadtxt(filename, delimiter=",") # Load data from file
-
-    lam = data[:, 0] # Slice file array into x and y
+def plot_cal_norm(filename: str, save_image=False, save_data=False) -> None:
+    data = np.loadtxt(filename, delimiter=",")  # Load data from file
+    lam = data[:, 0]  # Slice file array into x and y
     intens = data[:, 1]
 
-    intens = intens - np.min(intens.nonzero()) # Calibrate. Subtract min zon-zero value
-    intens = intens / intens.max() # Normalize. Divide by greatest value so everything is scaled the same
+    # Calibrate. Subtract min zon-zero value
+    intens = intens - np.min(intens.nonzero())
+    # Normalize. Divide by greatest value so everything is scaled the same
+    intens = intens / intens.max()
 
-    pyplot.semilogy(lam, intens) # Plot with y log x lin
+    pyplot.semilogy(lam, intens)  # Plot with y log x lin
     pyplot.xlabel(r"Wavelength (nm)")
     pyplot.ylabel(r"Intensity (arbitrary)")
-    
-    if (save):
-        pyplot.savefig(filename.removesuffix(".csv") + ".png")
 
-    
+    if (save_image):
+        pyplot.savefig(filename.removesuffix(".csv") + ".png")
+    if (save_data):
+        out = np.ndarray(data.shape)
+        out[:, 0] = lam
+        out[:, 1] = intens
+        print(out)
+        np.savetxt(filename.removesuffix(".csv") + "-PROCESSED" +
+                   ".csv", out, delimiter=",", fmt='%f')
+
     pyplot.show()
-    
-plot_cal_norm("PHYS 2610H/Lab 3/He/He_VIS_10_0.5nm_250um_350-500.csv")
+
+
+plot_cal_norm(
+    "PHYS 2610H/Lab 3/He/He_VIS_100_0.1nm_250um_350-750.csv", save_data=True)
