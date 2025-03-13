7980525e76a291728c64942f3da3725635c24bab
MrJeremyFisher <jeremy@generaleclectic.org>
2025-03-13 15:46:02 -0400

    More work

--- PHYS 2610H/Lab 3/plotter.py
+++ PHYS 2610H/Lab 3/plotter.py
@@ -2,27 +2,27 @@ from matplotlib import pyplot
 import numpy as np
 
 
-def plot_cal_norm(filename: str, save_image=False, save_data=False) -> None:
+def plot_cal_norm(filename: str, plot=False, save_image=False, save_data=False) -> None:
     data = np.loadtxt(filename, delimiter=",")  # Load data from file
     lam = data[:, 0]  # Slice file array into x and y
     intens = data[:, 1]
 
     # Calibrate. Subtract min zon-zero value
-    intens = intens - np.min(intens.nonzero())
+    intens = intens - \
+        np.min(np.ma.masked_array(intens, mask=intens == 0).min())
     # Normalize. Divide by greatest value so everything is scaled the same
     intens = intens / intens.max()
 
-    pyplot.semilogy(lam, intens)  # Plot with y log x lin
-    pyplot.xlabel(r"Wavelength (nm)")
-    pyplot.ylabel(r"Intensity (arbitrary)")
-
-    if (save_image):
-        pyplot.savefig(filename.removesuffix(".csv") + ".png")
+    if (plot):
+        pyplot.plot(lam, intens)  # Plot with y log x lin
+        pyplot.xlabel(r"Wavelength (nm)")
+        pyplot.ylabel(r"Intensity (arbitrary)")
+        if (save_image):
+            pyplot.savefig(filename.removesuffix(".csv") + ".png")
     if (save_data):
         out = np.ndarray(data.shape)
         out[:, 0] = lam
         out[:, 1] = intens
-        print(out)
         np.savetxt(filename.removesuffix(".csv") + "-PROCESSED" +
                    ".csv", out, delimiter=",", fmt='%f')
 
@@ -30,4 +30,4 @@ def plot_cal_norm(filename: str, save_image=False, save_data=False) -> None:
 
 
 plot_cal_norm(
-    "PHYS 2610H/Lab 3/He/He_VIS_100_0.1nm_250um_350-750.csv", save_data=True)
+    "PHYS 2610H/Lab 3/Psi/Doublets/Psi_VIS_100_0.02nm_0um_587-591.csv", plot=True)
---
+++ PHYS 2610H/Lab 3/predictor.py
@@ -0,0 +1,31 @@
+import collections
+import pandas as pd
+
+S1 = [1.9663, 1.9094, 1.66277, 1.84869]
+S3 = [1.93347, 1.90298, 1.83241, 1.59856]
+P1 = [1.93949, 1.91493, 1.86209, 1.71135]
+P3 = [1.93808, 1.91217, 1.85564, 1.69087]
+D1 = [1.93925, 1.91447, 1.86105]
+D3 = [1.93925, 1.91444, 1.86102]
+
+levels_list = dict()
+
+
+def levels(E1, E2, E1n, E2n):
+    for i in range(len(E1)):
+        for j in range(len(E2)):
+            dE = abs(E1[i]-E2[j])
+            if dE > 0:
+                wavelength = 1/dE*100
+                if wavelength > 370 and wavelength < 750:
+                    levels_list.update(
+                        {"1s{}s{}-1s{}p{}".format(i, E1n, j, E2n): wavelength})
+
+
+levels(S1, P1, "1S", "1P")
+levels(P1, D1, "1P", "1D")
+levels(S3, P3, "3S", "3P")
+levels(P3, D3, "3P", "3D")
+pd.DataFrame.from_dict(data=levels_list, orient="index").to_csv(
+    "PHYS 2610H/Lab 3/predicted_vals.csv", header=False)
+print(len(levels_list))
