1fef89551f059990893222fa3ed0b65c88031747
MrJeremyFisher <jeremy@generaleclectic.org>
2025-10-01 17:11:31 -0400

    .

--- PHYS/PHYS 4050H/sinegen/sinegen.ino
+++ PHYS/PHYS 4050H/sinegen/sinegen.ino
@@ -1,6 +1,3 @@
-/*
-    1e6/(F*N_Samp) (us)
-*/
 #include <SPI.h>
 #define SAMPLES 16
 #define FREQUENCY 1000
@@ -12,44 +9,49 @@ uint16_t phase = 0;
 
 uint16_t SIN_LUT[SAMPLES] = {};
 
-void setup() {
+void setup()
+{
   // Populate lookup table @ ~1V_pp
-  for (int k = 0; k < SAMPLES; k++) {
+  for (int k = 0; k < SAMPLES; k++)
+  {
     SIN_LUT[k] = ((2048 / 2) * (sin(2 * PI * (float)k / SAMPLES) + 1));
   }
 
   SPI.begin();
 
-  // Set CS pin high to deselect the DAC
+  // Chip select pin active low, this will deselect the DAC
   digitalWrite(CS_PIN, HIGH);
 }
 
-void loop() {
+void loop()
+{
   uint32_t now = micros();
 
-  if (now > next) {
-    writeDAC(SIN_LUT[phase], 0);
+  if (now > next)
+  {
+    writeDAC(SIN_LUT[phase]);
 
     phase = (phase + 1) % SAMPLES;
     next += PERIOD;
   }
 }
 
-void writeDAC(uint16_t value, int channel) {
-  uint16_t command = 0x3000;  // Default command bits (0b0011 0000 0000 0000)
+void writeDAC(uint16_t value)
+{
+  uint16_t command = 0x3000; // 0011...,
+  // 0 -> Write Channel A
+  // 0 -> DC
+  // 1 -> V_o=V_r * value/4096
+  // 1 -> Output shutdown, operating on HI
+  // See datasheet for more
 
-  if (channel == 1) {
-    command |= 0x8000;  // Set bit 15 to select channel B
-  }
+  // OR command w/ masked value, gives
+  // 0011 CMD
+  command |= (value & 0x0FFF);
 
-  command |= (value & 0x0FFF);  // Mask the value to 12 bits and combine with the command
-
-  // Select the DAC by pulling CS low
   digitalWrite(CS_PIN, LOW);
 
-  // Send the command via SPI
   SPI.transfer16(command);
 
-  // Deselect the DAC by pulling CS high
   digitalWrite(CS_PIN, HIGH);
 }
