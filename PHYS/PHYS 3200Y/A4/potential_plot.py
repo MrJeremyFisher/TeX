bb488755ed8c55375a09f97aae17661729de5644
MrJeremyFisher <jeremy@generaleclectic.org>
2025-11-25 20:42:41 -0500

    .

--- PHYS/PHYS 3200Y/A4/potential_plot.py
+++ PHYS/PHYS 3200Y/A4/potential_plot.py
@@ -5,18 +5,22 @@ E_0 = 1
 a = 1
 resolution = 500
 
-
-
 x = np.linspace(-5,5,resolution)
 y = np.linspace(-5,5,resolution)
 X, Y = np.meshgrid(x, y)
 Z = E_0 * (-(np.sqrt(X**2 + Y**2)) + a**2 /
            np.sqrt(X**2 + Y**2)) * np.cos(np.arctan(Y / X))
 
+# E = np.gradient(Z)
+
 levels = np.linspace(-15, 0, 300)
-pyplot.contour(X, Y, Z, levels=levels, cmap="jet")
+pyplot.contourf(X, Y, Z, levels=levels, cmap="jet")
 phi =  np.linspace(0, 2 * np.pi, 20)
 pyplot.plot(a*np.cos(phi),a*np.sin(phi), color="black", linestyle="dashed")
+
+# step = 10
+# s = max(np.max(E[0]), np.max(E[1])) * 0.1
+# pyplot.quiver(X[::step,::step], Y[::step,::step], E[1][::step,::step], E[0][::step,::step], units='xy', pivot='tail', width=0.03, scale=1)
 pyplot.axis("equal")
 pyplot.colorbar()
 pyplot.show()
--- PHYS/PHYS 3200Y/Lab 2B/real data.py
+++ PHYS/PHYS 3200Y/Lab 2B/real data.py
@@ -1,32 +1,57 @@
 import numpy as np
 from matplotlib import pyplot as plt
 
-data = np.loadtxt("PHYS/PHYS 3200Y/Lab 2B/SDS00023.csv",
-                  skiprows=2, delimiter=",")
+freqs = []
+caps = []
+res = []
 
-t = data[:, 0]
-V = data[:, 1]
-V1 = data[:, 2]
+for i in range(1, 27):
+    data = np.loadtxt(f"PHYS/PHYS 3200Y/Lab 2B/real data/SDS000{i:02d}.csv",
+                      skiprows=12, delimiter=",")
 
-a = np.fft.rfft(V)
-nf = a.size
+    t = data[:, 0]
+    V = data[:, 1]
+    V1 = data[:, 2]
 
-dt = t[1] - t[0]
-N = len(t)
-f = np.arange(nf) / (N * dt)
+    a = np.fft.rfft(V)
+    a1 = np.fft.rfft(V1)
+    nf = a.size
+
+    dt = t[1] - t[0]
+    N = len(t)
+    # plt.figure(i-1)
+    f = np.arange(nf) / (N * dt)
+    # fig, axes = plt.subplots(2, 3)
+
+    # for i in range(0, 3):
+    #     axes[0,i].set_xscale("log")
+
+    # axes[0, 0].plot(f, a.real)
+    # axes[0, 0].set_title("real")
+    # axes[0, 1].plot(f, a.imag)
+    # axes[0, 1].set_title("imag")
+    # axes[0, 2].plot(f, np.abs(a))
+    # axes[0, 2].set_title("abs")
+
+    # axes[1, 0].plot(t, V, label="proc")
+    # axes[1, 2].plot(t, V1, label="ref")
+
+    index = np.argmax(np.abs(a))
+    freqs.append(f[index])
+    capacitance = (((a1/a)-1)*(0.22*10**-6))[index]
+    caps.append(capacitance)
+    resistance = (-2*np.pi*f[index]*np.imag(capacitance))**-1
+    res.append(resistance)
+    # fig.suptitle(f"{f[np.argmax(np.abs(a))]}Hz - {capacitance}F")
+    # plt.legend()
+
 fig, axes = plt.subplots(1, 3)
+axes[0].plot(freqs, np.imag(caps))
+axes[0].set_xscale("log")
 
-axes[0].plot(f, a.real)
-axes[0].set_title("real")
-axes[1].plot(f, a.imag)
-axes[1].set_title("imag")
-axes[2].plot(f, np.abs(a))
-axes[2].set_title("abs")
+axes[1].plot(freqs, np.real(caps))
+axes[1].set_xscale("log")
 
-plt.setp(axes, xscale="log")
-plt.figure(2)
-plt.plot(t, V, label="proc")
-plt.plot(t, V1, label="ref")
-plt.title("Data")
-plt.legend()
+axes[2].plot(freqs, res)
+axes[2].set_xscale("log")
 plt.show()
